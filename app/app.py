c27620129c0154d66b84c81c8203a3f42326300f
Daniel <daniel@pont.in>
2016-06-27 21:43:04 +0100

    Add initial theme support.

--- app/app.py
+++ app/app.py
@@ -1,22 +1,48 @@
 import os
-from os import listdir
-from os.path import isfile, join
-from flask import Flask, render_template, flash, redirect, request, url_for
+import jinja2
+from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 app.config.from_object('config')
+theme = "default"
+
+theme_loader = jinja2.ChoiceLoader([
+    app.jinja_loader,
+    jinja2.FileSystemLoader(['themes/%s/templates/' % theme,
+                             'themes/default/templates/']),
+])
+app.jinja_loader = theme_loader
+
+
+@app.route('/base/<path:filename>')
+def theme_static(filename):
+    return send_from_directory(app.root_path + "/themes/%s/static/" % theme, filename)
 
 
 @app.route("/")
 def index():
     return render_template("index.html",
+                           title='Home')
+
+
+@app.route("/admin")
+def admin():
+    listThemes()
+    return render_template("admin.html",
                            title='Home',
-                           current_css=app.config['CURRENT_CSS'])
+                          )
 
 
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('errors/404.html'), 404
+
+
+def listThemes():
+    for subdir, dirs, files in os.walk("themes"):
+        for file in files:
+            print(os.path.join(subdir, file), file=sys.stderr)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/config.py
+++ app/config.py
@@ -1,6 +1,3 @@
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
-CURRENT_CSS = 'default.css'
-CSS_UPLOAD_FOLDER = './app/static/stylesheets/'
-ALLOWED_EXTENSIONS = set(['css'])
