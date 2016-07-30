a778da05f701cdecea676d8b4b32f35675da2aa0
NickBrisebois <nickbrisebois12@gmail.com>
2016-07-30 15:14:05 -0400

    Added dropdown for folders

--- app/app.py
+++ app/app.py
@@ -25,7 +25,10 @@ def page(path):
         nav_items = []
         for page in os.listdir("pages"):
             if page.endswith('.md'):
-                page_dict = {'name': page, 'path': '/'+page}
+                page_dict = {'type': 'file', 'name': page, 'path': '/'+page}
+                nav_items.append(page_dict)
+            elif os.path.isdir("pages/"+page):
+                page_dict = {'type': 'subfolder', 'name': page, 'path': "#"}
                 nav_items.append(page_dict)
 
         return render_template("base.html",
